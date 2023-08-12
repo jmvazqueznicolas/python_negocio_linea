393bdb8d4ef2ae11d1f65c5a1bf41ab0456df397
Jesus M Vazquez <jmvazqueznicolas@gmail.com>
2023-08-12 13:15:41 -0600

    Se agrego la app de tienda al proyecto

--- mi_negocio_web/settings.py
+++ mi_negocio_web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'nucleo',
+    'tienda',
 ]
 
 MIDDLEWARE = [
--- tienda/views.py
+++ tienda/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1>Mi negocio web</h1>")
+def tienda_inicio(request):
+    return HttpResponse("<h1>Bienvenido a la tienda</h1>")
