6e1de7fb86afbf6702fa7b3e161c7ee0ae8c2a8f
Jesus M Vazquez <jmvazqueznicolas@gmail.com>
2023-08-19 13:48:07 -0600

    Catalogo terminado se agregaron vistas y urls

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'tienda'
 
 urlpatterns = [
-    path ('', views.tienda_inicio, name='tienda'),
+    path('', views.lista_productos, name='lista_productos'),
+    path('<slug:categoria_slug>/', views.lista_productos, name='lista_productos_por_categoria'),
+    path('<int:id>/<slug:slug>', views.producto_detalle, name="producto_detalle"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Categoria, Producto
 
 # Create your views here.
 def tienda_inicio(request):
     return HttpResponse("<h1>Bienvenido a la tienda</h1>")
+
+def lista_productos(request, categoria_slug=None):
+    categoria = None
+    categorias = Categoria.objects.all()
+    productos = Producto.objects.filter(disponibilidad=True)
+    if categoria_slug:
+        categoria = get_object_or_404(Categoria, slug=categoria_slug)
+        productos = productos.filter(categoria=categoria)
+    return render(request, 'tienda/productos/lista.html', 
+                  {'categoria': categoria,
+                   'categorias': categorias,
+                   'productos': productos})
+
+def producto_detalle(request, id, slug):
+    producto = get_object_or_404(Producto, 
+                                 id=id,
+                                 slug=slug,
+                                 disponibilidad=True)
+    return render(request, 'tienda/productos/detalles.html',
+                  {'producto': producto})
