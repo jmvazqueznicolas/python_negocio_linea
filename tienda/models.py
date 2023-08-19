44ae8aabdf3301e410dc60a4feb0df760282ee04
Jesus M Vazquez <jmvazqueznicolas@gmail.com>
2023-08-19 12:42:04 -0600

    Se crearon modelos para producto y categoria

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200,
+                              db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique = True)
+    
+    class Meta:
+        ordering = ('nombre',)
+        verbose_name = 'Categoria'
+        verbose_name_plural = "Categorias"
+
+    def __str__(self):
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return reverse('tienda:lista_productos_por_categoria',
+                       args=[self.slug])
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='productos',
+                                on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    imagen = models.ImageField(upload_to='productos/%Y/%m/%d', blank=True)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    disponibilidad = models.BooleanField(default=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('nombre',)
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return reverse('tienda:producto_detalle', 
+                        args=[self.id, self.slug])
+    
