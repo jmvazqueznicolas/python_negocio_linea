2749b11436fd315171fba7d4efb1981206e6dd7b
Jesus M Vazquez <jmvazqueznicolas@gmail.com>
2023-08-26 13:24:41 -0600

    Se agrego boton al html de la tienda

--- tienda/views.py
+++ tienda/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Categoria, Producto
+from carrito.formulario import FormularioAgregarProducto
 
-# Create your views here.
 def tienda_inicio(request):
     return HttpResponse("<h1>Bienvenido a la tienda</h1>")
 
@@ -13,15 +14,15 @@ def lista_productos(request, categoria_slug=None):
     if categoria_slug:
         categoria = get_object_or_404(Categoria, slug=categoria_slug)
         productos = productos.filter(categoria=categoria)
-    return render(request, 'tienda/productos/lista.html', 
-                  {'categoria': categoria,
-                   'categorias': categorias,
-                   'productos': productos})
+    return render(request, 'tienda/productos/lista.html',
+                 {'categoria': categoria,
+                  'categorias': categorias,
+                  'productos': productos})
 
 def producto_detalle(request, id, slug):
-    producto = get_object_or_404(Producto, 
-                                 id=id,
-                                 slug=slug,
-                                 disponibilidad=True)
-    return render(request, 'tienda/productos/detalles.html',
-                  {'producto': producto})
+    producto = get_object_or_404(Producto, id=id,
+                                slug=slug, disponibilidad=True)
+    form_carrito_producto = FormularioAgregarProducto
+    return render(request, 'tienda/productos/detalles.html', 
+                  {'producto': producto,
+                   'form_carrito_producto': form_carrito_producto})
